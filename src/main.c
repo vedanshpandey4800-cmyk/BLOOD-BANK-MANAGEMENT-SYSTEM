4c4f15535aada0bef8882dd1f75f5cd4019cf8d2
vedanshpandey4800-cmyk <vedanshpandey4800@gmail.com>
2025-11-28 11:59:42 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "blood_bank.h"
 
+int main() {
+    printf(" BLOOD BANK MANAGEMENT SYSTEM \n");
+    printf("============================\n");
+    
+    int choice;
+    while(1) {
+        printf("\n1. Add Donor\n");
+        printf("2. View Donors\n");
+        printf("3. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice) {
+            case 1: addDonor(); break;
+            case 2: displayDonors(); break;
+            case 3: exit(0);
+            default: printf(" Invalid choice!\n");
+        }
+    }
+    return 0;
+}
