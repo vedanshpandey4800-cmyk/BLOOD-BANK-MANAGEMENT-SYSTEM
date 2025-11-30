173332de7beee71c18f7d04b921c247b8839c253
vedanshpandey4800-cmyk <vedanshpandey4800@gmail.com>
2025-11-30 11:02:45 +0530

    Update blood_bank.h

--- include/blood_bank.h
+++ include/blood_bank.h
@@ -1,14 +1,19 @@
 #ifndef BLOOD_BANK_H
 #define BLOOD_BANK_H
 
+#define MAX_DONORS 100
+
 struct Donor {
-    int    id;
-    char   fullName[50];
-    char   bGroup[5];
-    char   phoneNo[15];
-    int    yearsOld;
-    char   homeAddr[100];
+    int id;
+    char name[50];
+    char bloodGroup[5];
+    char phone[15];
+    int age;
+    char address[100];
 };
+
+extern struct Donor donors[MAX_DONORS];
+extern int donorCount;
 
 void addDonor();
 void displayDonors();
